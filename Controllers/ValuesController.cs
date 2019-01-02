3550ac5dbed0020b507d1954ba4dc02edd23e171
kiranpatekar <33060145+kiranpatekar@users.noreply.github.com>
2019-01-02 20:27:07 +0530

    Add files via upload

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sendingMail_mailkit.Services;
+using sendingMail_mailkit.Model;
+namespace sendingMail_mailkit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+            IEmailService EmailService;
+            public ValuesController(IEmailService EmailService)
+            {
+                this.EmailService = EmailService;
+            }
+
+   [HttpPost("contactUs")] 
+         public void c([FromBody]EmailMessage e)
+        {
+
+               EmailService.Send(e);
+                   
+        }
+
+    }
+}
